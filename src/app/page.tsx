59c1b5a3785cbed4fbc19b49d1453c84d906b157
Johan van Eck <jve.software@gmail.com>
2025-08-04 08:22:50 +0200

    Add animate-pulse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-xl">Van Eck Photography</h1>
+      <h1 className="text-xl animate-pulse">Van Eck Photography</h1>
     </div>
   );
 }
