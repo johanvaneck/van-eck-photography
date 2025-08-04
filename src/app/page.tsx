1633fc3ea4fd39efe7883a4aa9656447f46fae1c
Johan van Eck <jve.software@gmail.com>
2025-08-04 08:41:03 +0200

    Test deployments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-xl animate-pulse">Van Eck Photography</h1>
-      <p>Test</p>
+      <p>Capturing moments in time</p>
     </div>
   );
 }
